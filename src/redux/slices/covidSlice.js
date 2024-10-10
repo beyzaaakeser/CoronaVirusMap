4597ee8e3855b34ccf02f41a52ba9ed164d4efce
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-10 16:59:51 +0300

    slice & reducer added

---
+++ src/redux/slices/covidSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState ={}
+
+const covidSlice = createSlice({
+  name: 'covid',
+  initialState: {},
+  reducers: {},
+});
+
+export default covidSlice.reducer;
