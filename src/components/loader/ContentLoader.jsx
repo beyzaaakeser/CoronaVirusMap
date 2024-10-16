cb1f1a2a57cfe34545d6b461d021189d683db7d1
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-16 19:07:02 +0300

    updated

--- src/components/loader/ContentLoader.jsx
+++ src/components/loader/ContentLoader.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const ContentLoader = () => {
-  return (
-    <>
-        <div data-testid="card-loader"></div>
-        <div data-testid="card-loader"></div>
-        <div data-testid="card-loader"></div>
-        <div data-testid="card-loader"></div>
-        <div data-testid="card-loader"></div>
-    </>
-  )
-}
+  const arr = new Array(10).fill('');
+  return arr.map((_, key) => (
+    <div
+    key={key}
+      data-testid="card-loader"
+      className="p-5 border text-black shadow rounded-md text-transparent select-none bg-gray-100 animate-pulse "
+    >
+      <div className="text-sm mb-2 w-20 rounded-lg bg-gray-400">.</div>
+      <div className="text-lg w-3/4 md:w-44 rounded-lg bg-gray-400">.</div>
+    </div>
+  ));
+};
 
-export default ContentLoader
+export default ContentLoader;
