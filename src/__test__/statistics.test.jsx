48ab2b421c94fae77ed5027294cf16b2b845be76
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-17 18:30:44 +0300

    statistics test updated

--- src/__test__/statistics.test.jsx
+++ src/__test__/statistics.test.jsx
@@ -5,50 +5,54 @@ import millify from 'millify';
 
 jest.mock('../api');
 
-test('renders the loading state initially', async () => {
-  api.get.mockResolvedValueOnce({ data: [] });
+describe('Statistics Component Tests', () => {
+  beforeEach(()=>{jest.clearAllMocks()});
 
-  render(<Statistics />);
+  test('renders the loading state initially', async () => {
+    api.get.mockResolvedValueOnce({ data: [] });
 
-  const loader = screen.getByTestId('home-loader');
-});
+    render(<Statistics />);
 
-test("'displays statistics after loader'", async () => {
-  const totals = {
-    confirmed: 701166431,
-    recovered: 590451974,
-    critical: 2877,
-    deaths: 7014639,
-    lastChange: '2024-06-04T00:28:51+00:00',
-    lastUpdate: '2024-08-09T09:13:41+00:00',
-  };
+    const loader = screen.getByTestId('home-loader');
+  });
 
-  api.get.mockResolvedValueOnce({ data: [totals] });
+  test("'displays statistics after loader'", async () => {
+    const totals = {
+      confirmed: 701166431,
+      recovered: 590451974,
+      critical: 2877,
+      deaths: 7014639,
+      lastChange: '2024-06-04T00:28:51+00:00',
+      lastUpdate: '2024-08-09T09:13:41+00:00',
+    };
 
-  render(<Statistics />);
+    api.get.mockResolvedValueOnce({ data: [totals] });
 
-  await waitFor(() =>
-    expect(screen.queryByTestId('home-loader')).not.toBeInTheDocument()
-  );
+    render(<Statistics />);
 
-  expect(screen.getByText(/Total Cases/i));
-  expect(screen.getByText(millify(totals.confirmed)));
+    await waitFor(() =>
+      expect(screen.queryByTestId('home-loader')).not.toBeInTheDocument()
+    );
 
-  expect(screen.getByText(/Total Recovered cases/i));
-  expect(screen.getByText(millify(totals.recovered)));
+    expect(screen.getByText(/Total Cases/i));
+    expect(screen.getByText(millify(totals.confirmed)));
 
-  expect(screen.getByText(/Total deaths/i));
-  expect(screen.getByText(millify(totals.deaths)));
-});
+    expect(screen.getByText(/Total Recovered cases/i));
+    expect(screen.getByText(millify(totals.recovered)));
 
-test('displays error message when statistics cannot be fetched', async () => {
-  api.get.mockRejectedValueOnce(new Error('Request timeout.'));
+    expect(screen.getByText(/Total deaths/i));
+    expect(screen.getByText(millify(totals.deaths)));
+  });
 
-  render(<Statistics />);
+  test('displays error message when statistics cannot be fetched', async () => {
+    api.get.mockRejectedValueOnce(new Error('Request timeout.'));
 
-  await waitFor(() =>
-    expect(screen.queryByTestId('home-loader')).not.toBeInTheDocument()
-  );
+    render(<Statistics />);
 
-  screen.getByText("Statistics cannot be obtained.")
+    await waitFor(() =>
+      expect(screen.queryByTestId('home-loader')).not.toBeInTheDocument()
+    );
+
+    screen.getByText('Statistics cannot be obtained.');
+  });
 });
